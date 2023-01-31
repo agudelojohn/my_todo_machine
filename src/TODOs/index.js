cfbeb321fe5e7ed805bbc2fade663ff8a14c2a07
John Agudelo <jhbr08@hotmail.com>
2023-01-30 21:17:30 -0500

    Modal shape created.

--- src/TODOs/index.js
+++ src/TODOs/index.js
@@ -1,14 +1,24 @@
-import React, { Fragment } from "react";
+import React from "react";
+
+import { Grid, Box } from "@mui/material";
 
 import { TodoHeader } from "./TODOHeader";
 import { TodoList } from "./TODOList";
+import { TodoModal } from "./TODOModal";
 
 function TodoCounter() {
   return (
-    <Fragment>
-      <TodoHeader />
-      <TodoList itemList={[1, 2, 3, 4]} />
-    </Fragment>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <TodoModal />
+        </Grid>
+        <Grid item xs={6}>
+          <TodoHeader />
+          <TodoList itemList={[1, 2, 3, 4]} />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 

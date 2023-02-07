93a1297b485bcb17ca400635c71e760d95e730be
John Agudelo <jhbr08@hotmail.com>
2023-02-06 20:05:54 -0500

    Header of TODOs list shape

--- src/components/TODOs/TODOHeader/index.js
+++ src/components/TODOs/TODOHeader/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Paper, Typography } from "@mui/material";
 
-function TodoHeader() {
-  return <h1>Has completado 25 de 54 TODOs </h1>;
+function TodoHeader({ done, total }) {
+  return (
+    <Paper elevation={3} sx={{ textAlign: "center", padding: "2rem" }}>
+      <Typography variant="h1">Your tasks</Typography>
+      <Typography variant="h4">
+        Completed {done} of {total}
+      </Typography>
+    </Paper>
+  );
 }
 
 export { TodoHeader };
--- src/components/TODOs/index.js
+++ src/components/TODOs/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Grid, Box } from "@mui/material";
 
@@ -7,15 +7,25 @@ import { TodoList } from "./TODOList";
 import { TodoModal } from "./TODOModal";
 
 function TodoCounter() {
+  const [todoList, setTodoList] = useState([1, 2, 3, 4, 5, 6, 7]);
+  const [todosDone, setTodosDone] = useState(0);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
+      <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid item xs={6}>
           <TodoModal />
         </Grid>
         <Grid item xs={6}>
-          <TodoHeader />
-          <TodoList itemList={[1, 2, 3, 4]} />
+          <Grid
+            container
+            direction="column"
+            spacing={2}
+            sx={{ maxWidth: "90%", margin: "auto", paddingTop: "2rem" }}
+          >
+            <TodoHeader done={todosDone} total={todoList.length} />
+            <TodoList itemList={todoList} />
+          </Grid>
         </Grid>
       </Grid>
     </Box>
